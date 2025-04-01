32b371c23cf52b3b343f98de854afe2e8ca85dc4
Max Franco <maxcodefranco@gmail.com>
2025-04-01 03:10:27 -0300

    add postcss and autoprefixer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     content: [
+        './src/**/*.{js,ts,jsx,tsx}', // Include all source files
         './node_modules/@shadcn/ui/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
